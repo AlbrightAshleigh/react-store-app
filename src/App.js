b37876a4af665d200271a4ff1be6558ec809c0b8
ashleighalbright <albrightashleigh@gmail.com>
2019-08-29 13:02:16 -0400

    adding react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import {Route} from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component'
 
 function App() {
   return (
     <div>
+      <Route exact path='/' component= {HomePage}/>
       <HomePage/>
     </div>
   );
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -14,7 +14,7 @@ class Directory extends React.Component{
             title: 'hats',
             imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/3/3a/Cat03.jpg',
             id: 1,
-            linkUrl: 'shop/hats'
+            linkUrl: 'hats'
           },
           {
             title: 'jackets',
@@ -50,8 +50,8 @@ class Directory extends React.Component{
         return(
             <div className='directory-menu'>
                 {
-                    this.state.sections.map(({title,imageUrl, id, size}) =>(
-                        <MenuItem key={id} title={title} imageUrl ={imageUrl} size={size}/>
+                    this.state.sections.map(({id, ...otherSectionProps}) =>(
+                        <MenuItem key={id} {...otherSectionProps}/> 
                         )
                     )
                 }
