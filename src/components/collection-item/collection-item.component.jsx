051084dd578d26593da40f68f93877435f5e8729
ashleighalbright <albrightashleigh@gmail.com>
2019-09-03 10:21:58 -0400

    adding sign-in form

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -11,16 +11,13 @@ const CollectionItem = ({id, name, price, imageUrl}) => (
             backgroundImage: `url($imageUrl)`
 
         }}
-        >
+        />
         
         <div className='collection-footer'>
             <span className='name'>{name}</span>
             <span className='price'>{price}</span>
         </div>
         
-        </div>
-
-
     </div>
 
 );
